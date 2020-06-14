e32588a4e5fc24a373c2921ad09679e0b4f2b911
NietoCurcio <felipe_nieto010@hotmail.com>
2020-06-13 23:19:58 -0300

    second commit

---
+++ middleware/auth.js
@@ -0,0 +1,24 @@
+const jwt = require('jsonwebtoken');
+const config = require('config');
+
+module.exports = function (req, res, next) {
+  // access to req and res cycle, next to give the cycle to our routes
+  //   GET token from the header
+  const token = req.header('x-auth-token');
+  // req take, res send :D
+
+  if (!token) {
+    return res.status(401).json({ msg: 'No token, authorization denied' });
+  }
+
+  // Verify token
+  try {
+    const decoded = jwt.verify(token, config.get('jwtSecret'));
+
+    req.user = decoded.user;
+    // assign a value to user in request object
+    next();
+  } catch (err) {
+    res.status(401).json({ msg: 'Token is not valid' });
+  }
+};
