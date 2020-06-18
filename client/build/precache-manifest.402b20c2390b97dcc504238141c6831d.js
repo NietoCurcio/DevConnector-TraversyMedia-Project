66bcad47a87efb82379621361c9394b68451a29a
NietoCurcio <felipe_nieto010@hotmail.com>
2020-06-18 17:09:08 -0300

    Depoly to heroku

---
+++ client/build/precache-manifest.402b20c2390b97dcc504238141c6831d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c66766456f1b233c1b47eea073b073b8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3f70706cce668fdb8186",
+    "url": "/static/css/main.28ec31e2.chunk.css"
+  },
+  {
+    "revision": "306a32f50bbb835e1923",
+    "url": "/static/js/2.9518d529.chunk.js"
+  },
+  {
+    "revision": "603816eec26c1aff4a38bef8d08d40a6",
+    "url": "/static/js/2.9518d529.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3f70706cce668fdb8186",
+    "url": "/static/js/main.6cf283a2.chunk.js"
+  },
+  {
+    "revision": "13c5ce7c89dba05c4fba",
+    "url": "/static/js/runtime-main.8c40394c.js"
+  },
+  {
+    "revision": "4b31330b20e3cff3e49d540e4e7175e2",
+    "url": "/static/media/showcase.4b31330b.jpg"
+  }
+]);
